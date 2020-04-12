e4f264b44a982bfe4880b18aba977fa9dd828af7
Kushal Ganatra <kushrg2004@gmail.com>
2020-04-12 19:08:43 +0530

    Update converter.py
    Better way to write code

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
-print (" Enter your temperature in celcius ")
-celcius = float(input())
+
+celcius = float(input(" Enter your temperature in celcius "))
 ferenhiet = celcius * (9/5) + 32 
 print(f"your temperature in celcius is {celcius} and in ferenhiet is {ferenhiet}")
 
